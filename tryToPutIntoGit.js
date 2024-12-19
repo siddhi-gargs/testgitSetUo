944705730f57806c20ac230d2914f69e5e572206
siddhi-gargs <183721797+siddhi-gargs@users.noreply.github.com>
2024-12-19 13:14:19 +0530

    arrange According To Pivot

--- tryToPutIntoGit.js
+++ tryToPutIntoGit.js
@@ -1,5 +1,18 @@
-console.log("transmission of data .");
+const arr2 = [25, 56, 23, 10, 15, 18, 34];
+const num = 45;
+const [...combine] = [...arr1, num, ...arr2];
+console.log("combine", combine);
 
-const arr1 = [1, 3, [2, 4]];
-const arr2 = [1, 3, [2, 4]];
+const string = [[45, 56, 34], [45, 20, 45]];
+
+
+
+const arr1 = [12, 20, 34, 63, 45, 90, 56, 35];
+const comparator = function (pivot) {
+  return function(a, b) {
+    return a > pivot && b > pivot ? b - a : a - b;
+  }
+}
+
+const pattern = arr1.sort(comparator(45));
 
